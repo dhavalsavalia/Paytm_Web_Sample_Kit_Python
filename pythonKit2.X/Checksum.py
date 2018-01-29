acb700df7f851a26acd3ee994e222e4d09c20caa
Dhaval Savalia <dhaval.savalia6@gmail.com>
2018-01-29 12:15:14 +0530

    fix for-loop syntax on line-27

--- pythonKit2.X/Checksum.py
+++ pythonKit2.X/Checksum.py
@@ -24,9 +24,9 @@ def generate_checksum(param_dict, merchant_key, salt=None):
 
 def generate_refund_checksum(param_dict, merchant_key, salt=None):
     for i in param_dict:    
-    if("|" in param_dict[i]):
-        param_dict = {}
-        exit()
+        if("|" in param_dict[i]):
+            param_dict = {}
+            exit()
     params_string = __get_param_string__(param_dict)
     salt = salt if salt else __id_generator__(4)
     final_string = '%s|%s' % (params_string, salt)
